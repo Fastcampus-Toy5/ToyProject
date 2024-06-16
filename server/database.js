73f78d75d884840fd9e028e5d7be91dcf72db567
kimisadev27 <kimisadev27@gmail.com>
2024-06-17 00:30:41 +0900

    사용자 api 개발 완료

--- server/database.js
+++ server/database.js
@@ -8,9 +8,38 @@ database.serialize(() => {
     CREATE TABLE IF NOT EXISTS Users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       userId TEXT NOT NULL,
-      email TEXT NOT NULL,
-      password TEXT NOT NULL
-    )`)
+      password TEXT NOT NULL,
+      email TEXT,
+      name TEXT,
+      team TEXT,
+      position TEXT,
+      withdraw Boolean,
+      isAdmin Boolean DEFAULT false
+    );
+
+    CREATE TABLE IF NOT EXISTS notices (
+      no INTEGER PRIMARY KEY AUTOINCREMENT,
+      subject TEXT NOT NULL,
+      content TEXT NOT NULL,
+      regDate DATE NOT NULL,
+      imgUrl TEXT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS commutes (
+      no INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId TEXT NOT NULL,
+      startTime DATE NOT NULL,
+      endTime DATE
+    );
+
+    CREATE TABLE IF NOT EXISTS attends (
+      no INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId TEXT NOT NULL,
+      startDate DATE NOT NULL,
+      endDate DATE NOT NULL,
+      type TEXT NOT NULL
+    );
+  `)
 })
 
 export default database
