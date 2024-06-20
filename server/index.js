c4491f1eaf98d2e847321d90965b42448371b555
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-06-20 21:20:17 +0900

    main import 정리, json data 제거

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
-import fs from "fs";
-import db from "./database.js";
-import { STATUS_CODES } from "http";
-import cors from "cors";
 
-import userRouter from './routes/users.js'
-import noticeRouter from './routes/notice.js'
-import commuteRouter from './routes/commute.js'
-import attendRouter from './routes/attend.js'
+import attendRouter from "./routes/attend.js";
+import commuteRouter from "./routes/commute.js";
+import noticeRouter from "./routes/notice.js";
+import userRouter from "./routes/users.js";
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -22,40 +19,14 @@ const API_URL = {
   user: "/api/users",
   notice: "/api/notices",
   commute: "/api/commutes",
-  attend: "/api/attends"
-}
+  attend: "/api/attends",
+};
 
 // route
 app.use(API_URL.user, userRouter);
 app.use(API_URL.notice, noticeRouter);
 app.use(API_URL.commute, commuteRouter);
 app.use(API_URL.attend, attendRouter);
-
-
-app.get("/api/users.json", (req, res) => {
-  fs.readFile("./server/data/users.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading JSON file:", err);
-      return res.status(500).send({
-        status: "Internal Server Error",
-        message: err,
-        data: null,
-      });
-    }
-
-    try {
-      const jsonData = JSON.parse(data);
-      res.json(jsonData);
-    } catch (parseErr) {
-      console.error("Error parsing JSON file:", parseErr);
-      return res.status(500).send({
-        status: "Internal Server Error",
-        message: parseErr,
-        data: null,
-      });
-    }
-  });
-});
 
 app.listen(port, () => {
   console.log(`ready to ${port}`);
